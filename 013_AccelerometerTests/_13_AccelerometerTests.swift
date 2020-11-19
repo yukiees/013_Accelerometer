bcf7ee878724f2dfd31f60376dd6950d82feafea
yukiees <yukiii8989@gmail.com>
2020-11-20 08:42:29 +0900

    Initial Commit

---
+++ 013_AccelerometerTests/_13_AccelerometerTests.swift
@@ -0,0 +1,33 @@
+//
+//  _13_AccelerometerTests.swift
+//  013_AccelerometerTests
+//
+//  Created by 松島優希 on 2020/11/20.
+//
+
+import XCTest
+@testable import _13_Accelerometer
+
+class _13_AccelerometerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
